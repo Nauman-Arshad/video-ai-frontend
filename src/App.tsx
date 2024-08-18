8cc07c7cbb5ff6a5d3a8fa5c47c6cc2fb4ec949b
N.K <naumankhan642@gmail.com>
2024-08-18 18:05:30 +0500

    Env set

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { FormEvent, useEffect, useState } from "react";
 
-function randomIntFromInterval(min: number, max: number) { // min and max included
+function randomIntFromInterval(min: number, max: number) {
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
@@ -11,22 +11,25 @@ function App() {
   const [samples, setSamples] = useState<string[]>([]);
   const [activeSampleIndex, setActiveSampleIndex] = useState<null | number>(null);
 
+  const apiUrl = import.meta.env.VITE_API_URL;
+
   useEffect(() => {
     if (!samples.length) {
-      axios.get('http://localhost:8080/samples')
+      axios.get(`${apiUrl}/samples`)
         .then(response => {
           setSamples(response.data);
         });
     }
-  }, []);
+  }, [samples, apiUrl]);
 
   useEffect(() => {
     if (samples.length) {
       randomSample();
-      setInterval(() => {
+      const interval = setInterval(() => {
         randomSample();
         console.log('random now');
       }, 3000);
+      return () => clearInterval(interval);
     }
   }, [samples]);
 
@@ -40,13 +43,13 @@ function App() {
     ev.preventDefault();
     setLoadingMessage('Generating assets...');
     const assetsResponse = await axios.get(
-      'http://localhost:8080/create-story?url=' + encodeURIComponent(url)
+      `${apiUrl}/create-story?url=` + encodeURIComponent(url)
     );
     const id = await assetsResponse.data;
     setLoadingMessage('Preparing your video...');
-    const videoResponse = await axios.get('http://localhost:8080/build-video?id=' + id);
+    const videoResponse = await axios.get(`${apiUrl}/build-video?id=` + id);
     setLoadingMessage('');
-    window.location.href = 'http://localhost:8080/' + videoResponse.data;
+    window.location.href = `${apiUrl}/` + videoResponse.data;
   }
 
   return (
@@ -99,7 +102,7 @@ function App() {
                   opacity: samplesKey === activeSampleIndex ? '1' : '0',
                   transform: 'scaleX(1) scaleY(1) scaleZ(1) rotateX(0deg) rotateY(0deg) rotateZ(3deg) translateX(0px) translateY(0px) translateZ(0px) skewX(0deg) skewY(0deg)'
                 }}
-                src={'http://localhost:8080/' + sample + '/final.mp4'}></video>
+                src={`${apiUrl}/` + sample + '/final.mp4'}></video>
             ))}
           </div>
         </div>
